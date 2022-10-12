d901965df75ab326420068960e731e02b22bce02
Daniil-Tevs <daniil_tevs@mail.ru>
2022-10-12 21:57:31 +0200

    create folder movie, where will the additional files for the console application be stored add data.php: file with names of movies

---
+++ movie/data.php
@@ -0,0 +1,144 @@
+<?php
+
+$movies = [
+	[
+		"title" => "The Shawshank Redemption",
+		"release_year" => 1994,
+		"duration" => 144,
+		"age_restriction" => 16,
+		"rating" => 9.3
+	],
+	[
+		"title" => "The Godfather",
+		"release_year" => 1972,
+		"duration" => 175,
+		"age_restriction" => 16,
+		"rating" => 9.2
+	],
+	[
+		"title" => "The Dark Knight",
+		"release_year" => 2008,
+		"duration" => 152,
+		"age_restriction" => 14,
+		"rating" => 9.0
+	],
+	[
+		"title" => "12 Angry Men",
+		"release_year" => 1957,
+		"duration" => 96,
+		"age_restriction" => 16,
+		"rating" => 9.0
+	],
+	[
+		"title" => "Schindler's List",
+		"release_year" => 1993,
+		"duration" => 195,
+		"age_restriction" => 16,
+		"rating" => 8.9
+	],
+	[
+		"title" => "The Lord of the Rings: The Return of the King",
+		"release_year" => 2003,
+		"duration" => 201,
+		"age_restriction" => 12,
+		"rating" => 8.9
+	],
+	[
+		"title" => "Pulp Fiction",
+		"release_year" => 1994,
+		"duration" => 154,
+		"age_restriction" => 18,
+		"rating" => 8.9
+	],
+	[
+		"title" => "Il buono, il brutto, il cattivo",
+		"release_year" => 1966,
+		"duration" => 161,
+		"age_restriction" => 12,
+		"rating" => 8.8
+	],
+	[
+		"title" => "Fight Club",
+		"release_year" => 1999,
+		"duration" => 139,
+		"age_restriction" => 18,
+		"rating" => 8.8
+	],
+	[
+		"title" => "Forrest Gump",
+		"release_year" => 1994,
+		"duration" => 142,
+		"age_restriction" => 0,
+		"rating" => 8.8
+	],
+	[
+		"title" => "Inception",
+		"release_year" => 2010,
+		"duration" => 148,
+		"age_restriction" => 12,
+		"rating" => 8.8
+	],
+	[
+		"title" => "The Matrix",
+		"release_year" => 1999,
+		"duration" => 136,
+		"age_restriction" => 16,
+		"rating" => 8.7
+	],
+	[
+		"title" => "Goodfellas",
+		"release_year" => 1990,
+		"duration" => 146,
+		"age_restriction" => 16,
+		"rating" => 8.7
+	],
+	[
+		"title" => "One Flew Over the Cuckoo's Nest",
+		"release_year" => 1975,
+		"duration" => 133,
+		"age_restriction" => 16,
+		"rating" => 8.7
+	],
+	[
+		"title" => "Shichinin no samurai",
+		"release_year" => 1954,
+		"duration" => 227,
+		"age_restriction" => 0,
+		"rating" => 8.6
+	],
+	[
+		"title" => "Se7en",
+		"release_year" => 1995,
+		"duration" => 127,
+		"age_restriction" => 18,
+		"rating" => 8.6
+	],
+	[
+		"title" => "The Silence of the Lambs",
+		"release_year" => 1991,
+		"duration" => 118,
+		"age_restriction" => 18,
+		"rating" => 8.6
+	],
+	[
+		"title" => "Cidade de Deus",
+		"release_year" => 2002,
+		"duration" => 130,
+		"age_restriction" => 16,
+		"rating" => 8.6
+	],
+	[
+		"title" => "La vita e bella",
+		"release_year" => 1997,
+		"duration" => 116,
+		"age_restriction" => 0,
+		"rating" => 8.6
+	],
+	[
+		"title" => "It's a Wonderful Life",
+		"release_year" => 1946,
+		"duration" => 130,
+		"age_restriction" => 0,
+		"rating" => 8.6
+	]
+];
