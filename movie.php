03cb52feb1a121a43378f04450ba3cc810d27ee0
Daniil-Tevs <daniil_tevs@mail.ru>
2022-10-12 22:44:07 +0200

    movie.php: include file data.php, add a cycle where films suitable for this age are searched and displayed

--- movie.php
+++ movie.php
@@ -1,4 +1,6 @@
 <?php
+/** @var array $movies */
+include "./movie/data.php";
 
 $age = readline("Enter your age:");
 
@@ -12,7 +14,23 @@ else
 	if($age <0 || $age > 120)
 	{
 		echo "Invalid input: age must be between 0 and 100";
+
 	}
+	else
+	{
+		echo 'List of movies for your age:' . "\n";
+		$index = 1;
+		foreach($movies as $movie)
+		{
+			if($age>=$movie["age_restriction"])
+			{
+				echo "{$index}. {$movie["title"]} ({$movie["release_year"]}), {$movie["duration"]} minutes, {$movie["age_restriction"]}+, Rating - {$movie["rating"]}"
+					. "\n";
+				$index++;
+			}
+		}
+	}
+
 }
 
 
