28379a1f0c281f52d9d2cbcc1b447d7b504a1cfa
Daniil-Tevs <daniil_tevs@mail.ru>
2022-10-12 22:19:35 +0200

    movie.php: add age reading and input validation

--- movie.php
+++ movie.php
@@ -1,1 +1,21 @@
 <?php
+
+$age = readline("Enter your age:");
+
+if( !is_numeric($age))
+{
+	echo "Invalid input: entered not a number";
+}
+else
+{
+	$age = (int) $age;
+	if($age <0 || $age > 120)
+	{
+		echo "Invalid input: age must be between 0 and 100";
+	}
+}
+
+
+
+
+
