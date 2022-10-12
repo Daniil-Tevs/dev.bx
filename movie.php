f227d6474be84e9f0bc3f566306e3b1d2f313189
Daniil-Tevs <daniil_tevs@mail.ru>
2022-10-12 23:38:21 +0200

    movie.php: remove functions checkRightEnteredAge, printAvailableMovies, formatMovie to file .movie/functions.php

--- movie.php
+++ movie.php
@@ -5,33 +5,7 @@ declare(strict_types=1);
 /** @var string $title */
 include "./movie/data.php";
 include "functions.php";
-
-function checkRightEnteredAge($age):void
-{
-	if( !is_numeric($age))
-		exit("Invalid input: entered not a number");
-	if((int)$age <0 || (int)$age > 120)
-		exit("Invalid input: age must be between 0 and 120");
-
-}
-function formatMovie(array $movie):string
-{
-	return "{$movie["title"]} ({$movie["release_year"]}), {$movie["duration"]} minutes, {$movie["age_restriction"]}+, Rating - {$movie["rating"]}";
-}
-
-function printAvailableMovies(int $age,array $movies):void
-{
-	$index = 1;
-	foreach($movies as $movie)
-	{
-		if($age>=$movie["age_restriction"])
-		{
-
-			printMessage("{$index}." . formatMovie($movie));
-			$index++;
-		}
-	}
-}
+include "./movie/functions.php";
 
 $age = readline("Enter your age:");
 
