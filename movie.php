7eb6a2defad00c579ad5e2b56e93dd34c7eeaf73
Daniil-Tevs <daniil_tevs@mail.ru>
2022-10-12 23:21:30 +0200

    movie.php: add functions checkRightEnteredAge, printAvailableMovies, formatMovie

--- movie.php
+++ movie.php
@@ -2,36 +2,46 @@
 declare(strict_types=1);
 
 /** @var array $movies */
+/** @var string $title */
 include "./movie/data.php";
 
 function printMessage(string $message):void
 {
 	echo $message . "\n";
 }
-
-$age = readline("Enter your age:");
-
-if( !is_numeric($age))
-	printMessage("Invalid input: entered not a number");
-else
+function checkRightEnteredAge($age):void
 {
-	$age = (int) $age;
-	if($age <0 || $age > 120)
-		printMessage("Invalid input: age must be between 0 and 120");
-	else
+	if( !is_numeric($age))
+		exit("Invalid input: entered not a number");
+	if((int)$age <0 || (int)$age > 120)
+		exit("Invalid input: age must be between 0 and 120");
+
+}
+function formatMovie(array $movie):string
+{
+	return "{$movie["title"]} ({$movie["release_year"]}), {$movie["duration"]} minutes, {$movie["age_restriction"]}+, Rating - {$movie["rating"]}";
+}
+
+function printAvailableMovies(int $age,array $movies):void
+{
+	$index = 1;
+	foreach($movies as $movie)
 	{
-		printMessage('List of movies for your age:');
-		$index = 1;
-		foreach($movies as $movie)
+		if($age>=$movie["age_restriction"])
 		{
-			if($age>=$movie["age_restriction"])
-			{
-				printMessage("{$index}. {$movie["title"]} ({$movie["release_year"]}), {$movie["duration"]} minutes, {$movie["age_restriction"]}+, Rating - {$movie["rating"]}");
-				$index++;
-			}
+
+			printMessage("{$index}." . formatMovie($movie));
+			$index++;
 		}
 	}
 }
+
+$age = readline("Enter your age:");
+
+checkRightEnteredAge($age);
+printMessage($title);
+printAvailableMovies((int)$age,$movies);
+
 
 
 
