09b78621378d4823a4652f7dde765ac7faaa8c50
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 19:40:11 +0200

    menu.php: add component of menu navigation

---
+++ views/components/menu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var array $menu
+ */
+?>
+<nav>
+	<div class="icon">
+		<a href='/'><img src="data/icons/logo.png" alt="none"></a>
+	</div>
+	<div class="menu">
+		<?php foreach ($menu as $url=>$title):?>
+			<div class="menu-item"><a href="<?= $url?>"><?=$title?></a></div>
+		<?php endforeach?>
+	</div>
+</nav>
