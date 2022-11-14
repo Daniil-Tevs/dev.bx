18b32ca93adfcbb8c183413bc9ebb6de660c2e61
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 21:04:03 +0200

    movie_service.php: add function for search movie - get_movies_by_genre, get_movies_by_id

--- services/movie_service.php
+++ services/movie_service.php
@@ -8,3 +8,27 @@ function format_time(int $min):string
 function format_description(string $description, int $len = 310):string{
 	return (strlen($description)>$len)? substr($description,0,320)."...": $description;
 }
+
+function get_movies_by_id(int $id,array $movies):array{
+	foreach ($movies as $movie)
+	{
+		if($movie['id'] === $id)
+		{
+			return $movie;
+		}
+	}
+	return [];
+}
+
+function get_movies_by_genre(string $genre,array $movies):array{
+	$movies_of_genre = [];
+	foreach ($movies as $movie)
+	{
+		if(in_array($genre, $movie['genres'], true))
+		{
+
+			$movies_of_genre[] = $movie;
+		}
+	}
+	return $movies_of_genre;
+}
