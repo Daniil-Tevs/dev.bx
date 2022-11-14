8330596d98f07e46ba582d7e354e93eece36a8df
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 22:27:20 +0200

    favourite.php: add page for favourite movies

---
+++ public/favourite.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../boot.php';
+/**
+ * @var string $title
+ * @var int $description_len
+ * @var array $base_menu
+ */
+
+if (isset($_GET['genre']))
+{
+	redirect(sprintf('/genre=%s', $_GET['genre']));
+}
+
+echo view('layout', [
+	'title' => $title,
+	'menu' => $base_menu,
+	'content' => view('/pages/favourite',[]),
+]);
