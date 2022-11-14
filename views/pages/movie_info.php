54c47f83fab7952ac0fa4deb2843e2c84d2d7858
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 12:32:53 +0200

    movie.php: make page of details about the movie

---
+++ views/pages/movie_info.php
@@ -0,0 +1,86 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Я учусь в Б24</title>
+	<link rel="stylesheet" href="../../public/reset.css">
+	<link rel="stylesheet" href="../../public/style.css">
+</head>
+<body>
+
+<div class="container">
+	<div class="sidebar">
+		<nav>
+			<div class="icon">
+				<a href='/'><img src="../../data/icons/logo.png" alt="none"></a>
+			</div>
+			<div class="menu">
+				<div class="menu-item"><a href="/">Главная</a></div>
+				<div class="menu-item"><a href="/">Прочее</a></div>
+				<div class="menu-item"><a href="/">Избранное</a></div>
+			</div>
+		</nav>
+	</div>
+	<div class="wrapper">
+		<div class="header">
+			<form action="/" method="post">
+				<input type="text" name="title"  placeholder="Поиск по каталогу..." class="input-search">
+				<button type="submit" class="button-search">ИСКАТЬ</button>
+			</form>
+			<button type="submit" class="add-movie">ДОБАВИТЬ ФИЛЬМ</button>
+		</div>
+		<div class="content full_details">
+			<div class="movie-details">
+				<div class="ru-title">
+					<h1>Головоломка(2015)</h1>
+					<img src="../../data/icons/heart.png" alt="Heart">
+				</div>
+				<div class="en-title">
+					<h2>Inside brain</h2>
+					<div class="min-age">16&plus;</div>
+				</div>
+				<div class="base-section">
+					<img src="../../data/images/2.jpg" alt="poster">
+					<div class="description-section">
+						<div class="rating">
+							<div class="square active-square"></div>
+							<div class="square active-square"></div>
+							<div class="square active-square"></div>
+							<div class="square active-square"></div>
+							<div class="square active-square"></div>
+							<div class="square active-square"></div>
+							<div class="square"></div>
+							<div class="square"></div>
+							<div class="square"></div>
+							<div class="square"></div>
+							<div class="result">7.8</div>
+						</div>
+						<div class="base-info">
+							<h2>О фильме</h2>
+							<div class="year">
+								<h3>Год производства:</h3>
+								<p>2012</p>
+							</div>
+							<div class="directors">
+								<h3>Режиссер:</h3>
+								<p>2012</p>
+							</div>
+							<div class="actors">
+								<h3>В главных ролях:</h3>
+								<p>Эми Полер, Филлис Смит, Ричард Кайнд,  Билл Хейдер, Льюис Блэк, Минди Кейлинг</p>
+							</div>
+						</div>
+						<div class="full-description">
+							<h2>О фильме</h2>
+							<p>Райли — обычная 11-летняя школьница, и, как у каждого из нас, ее поведение определяют пять базовых эмоций: Радость, Печаль, Страх, Гнев и Брезгливость. Эмоции живут в сознании девочки и каждый день помогают ей справляться с проблемами, руководя всеми ее поступками. До поры до времени эмоции живут дружно, но вдруг оказывается, что Райли и ее родителям предстоит переезд из небольшого уютного городка в шумный и людный мегаполис. Каждая из эмоций считает, что именно она лучше прочих знает, что нужно делать в этой непростой ситуации, и в голове у девочки наступает полная неразбериха. Чтобы наладить жизнь в большом городе, освоиться в новой школе и подружиться с одноклассниками, эмоциям Райли предстоит снова научиться работать сообща.
+							Райли — обычная 11-летняя школьница, и, как у каждого из нас, ее поведение определяют пять базовых эмоций: Радость, Печаль, Страх, Гнев и Брезгливость. Эмоции живут в сознании девочки и каждый день помогают ей справляться с проблемами, руководя всеми ее поступками. До поры до времени эмоции живут дружно, но вдруг оказывается, что Райли и ее родителям предстоит переезд из небольшого уютного городка в шумный и людный мегаполис. Каждая из эмоций считает, что именно она лучше прочих знает, что нужно делать в этой непростой ситуации, и в голове у девочки наступает полная неразбериха. Чтобы наладить жизнь в большом городе, освоиться в новой школе и подружиться с одноклассниками, эмоциям Райли предстоит снова научиться работать сообща.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
