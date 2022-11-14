3464632aa2d4f9a8646cb5fa755d5031af54caf8
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 19:41:13 +0200

    movie_card.php: add component of menu card of info about movie in main page

---
+++ views/components/movie_card.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @var int $id
+ * @var string $title
+ * @var string $release_date
+ * @var string $original_title
+ * @var string $description
+ * @var int $duration
+ * @var array $genres
+ */
+?>
+<div class="movie-item">
+	<div class="movie-img" style="background-image: url('<?="/data/images/$id.jpg"?>')"></div>
+	<div class="movie-info">
+		<h1><?=sprintf('%s(%s)',$title,$release_date)?></h1>
+		<h2><?=$original_title?></h2>
+		<div class="movie-description">
+			<?= format_description($description) ?>
+		</div>
+		<div class="movie-additional-info">
+			<div class="movie-time">
+				<img src='/data/icons/clock.png' alt="Time:">
+				<p><?= sprintf('%s мин. / %s',$duration,format_time($duration))?></p>
+			</div>
+			<div class="movie-genre">
+				<?= implode(', ',array_slice($genres,0,3))?>
+			</div>
+		</div>
+	</div>
+</div>
