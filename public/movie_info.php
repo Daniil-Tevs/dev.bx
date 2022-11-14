191356752292034ccf4a5ce5d3b756c56b1f52dc
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 21:01:07 +0200

    movie_info.php: add content of page of info about the movie

---
+++ public/movie_info.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/../boot.php';
+/**
+ * @var string $title
+ * @var int $description_len
+ * @var array $movies
+ * @var array $genres
+ * @var array $base_menu
+ */
+$favour = array_pop($base_menu);
+foreach ($genres as $url => $genre)
+{
+	$base_menu['?q=' . $url] = $genre;
+}
+$base_menu[] = $favour;
+
+
+if (isset($_GET['id']) && is_int($_GET['id']))
+{
+	$movie = get_movies_by_id((int)$_GET['id'],$movies);
+}
+else
+{
+	$movie = [];
+}
+
+echo view('layout', [
+	'title' => $title,
+	'menu' => $base_menu,
+	'content' => view('pages/movie_info', [
+		'movies' => $movie,
+	])
+]);
