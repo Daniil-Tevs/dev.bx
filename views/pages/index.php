9f70c7e0c9959bb8fa5088c5fd13af37be3dbca8
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-14 19:42:30 +0200

    /pages/index.php: refactor, safe a content part

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,82 +1,15 @@
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Я учусь в Б24</title>
-	<link rel="stylesheet" href="../../public/reset.css">
-	<link rel="stylesheet" href="../../public/style.css">
-</head>
-<body>
+<?php
+/**
+ * @var string $menu
+ * @var array $movies
+ */
+?>
 
-<div class="container">
-	<div class="sidebar">
-		<nav>
-			<div class="icon">
-				<a href='/'><img src="../../data/icons/logo.png" alt="none"></a>
-			</div>
-			<div class="menu">
-				<div class="menu-item"><a href="/">Главная</a></div>
-				<div class="menu-item"><a href="/">Прочее</a></div>
-				<div class="menu-item"><a href="/">Избранное</a></div>
-			</div>
-		</nav>
-	</div>
-	<div class="wrapper">
-		<div class="header">
-			<form action="/" method="post">
-				<input type="text" name="title"  placeholder="Поиск по каталогу..." class="input-search">
-				<button type="submit" class="button-search">ИСКАТЬ</button>
-			</form>
-			<button type="submit" class="add-movie">ДОБАВИТЬ ФИЛЬМ</button>
-		</div>
-		<div class="content">
-			<div class="movie-item">
-				<div class="movie-img"></div>
-				<div class="movie-info">
-					<h1>Не время умирать(2021)</h1>
-					<h2>No time to die</h2>
-					<div class="movie-description">
-						dasdasdasdasdsdas
-					</div>
-					<div class="movie-additional-info">
-						<div class="movie-time">
-							<img src="../../data/icons/clock.png" alt="???">
-							<p>12312</p>
-						</div>
-						<div class="movie-genre">
-							Triller,Manga, Anime
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-			<div class="movie-item">
-				Contennnt
-			</div>
-
-		</div>
-	</div>
+<div class="content">
+	<?php
+	foreach ($movies as $movie): ?>
+		<?= view('components/movie_card', $movie) ?>
+	<?php
+	endforeach ?>
 </div>
 
-</body>
-</html>
