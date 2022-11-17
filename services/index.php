88bc2ccdb6b333618d82efcf4b3d2c97ab30cbdf
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-17 09:20:25 +0200

    services/index.php: require genre_service.php

--- services/index.php
+++ services/index.php
@@ -2,4 +2,5 @@
 
 require_once ROOT . '/services/template_service.php';
 require_once ROOT . '/services/movie_service.php';
+require_once ROOT . '/services/genre_service.php';
 require_once ROOT . '/services/http_service.php';
