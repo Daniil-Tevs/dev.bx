1ec8c524617270add6d252d146b69c03b14846a9
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-17 09:51:04 +0200

    public/index.php: add work with Post

--- public/index.php
+++ public/index.php
@@ -7,7 +7,19 @@ require_once __DIR__ . '/../boot.php';
  * @var array $genres
  * @var array $base_menu
  */
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+	$movie_title = trim($_POST['title']);
+	if (strlen($movie_title) > 0)
+	{
+		$movie_id = get_movie_id_by_title($movie_title,$movies);
+		redirect('/movie_info.php'."?id=$movie_id");
+	}
+	else
+	{
+		redirect('/');
+	}
+}
 if (isset($_GET['genre']))
 {
 	$genre = get_title_of_genre((string)$_GET['genre'],$genres);
