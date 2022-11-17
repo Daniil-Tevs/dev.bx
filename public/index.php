6b808b6002c872b9a346f09d9ea9df0b31c041b3
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-17 09:19:39 +0200

    public/index.php: remove var int $description_len, block else; add get title of genre from GET

--- public/index.php
+++ public/index.php
@@ -2,19 +2,16 @@
 
 require_once __DIR__ . '/../boot.php';
 /**
- * @var int $description_len
  * @var string $title
  * @var array $movies
+ * @var array $genres
  * @var array $base_menu
  */
 
 if (isset($_GET['genre']))
 {
-	$movies = get_movies_by_genre((string)$_GET['genre'], $movies);
-}
-else
-{
-	$movie = [];
+	$genre = get_title_of_genre((string)$_GET['genre'],$genres);
+	$movies = get_movies_by_title_of_genre($genre, $movies);
 }
 
 echo view('layout', [
