585f5dc5b0d0f8c8f695a1b98503b468af36fcb9
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 00:45:22 +0200

    genre_service.php: refactor function get_genres, add functions get_genre_id_by_url, get_genres_of_movie_id(

--- services/genre_service.php
+++ services/genre_service.php
@@ -1,6 +1,27 @@
 <?php
 
-function get_title_of_genre(string $url, array $genres):string
+function get_genres()
 {
-	return $genres[(string)$url];
+	$connection = get_db_connection();
+	$genres = mysqli_query($connection, "SELECT * FROM genre");
+	return ($genres) ? $genres : [];
+}
+
+function get_genre_id_by_url(string $url):int
+{
+	$connection = get_db_connection();
+	$id = mysqli_query($connection, "SELECT ID FROM genre WHERE CODE = '$url'");
+	$id = mysqli_fetch_assoc($id);
+	return ($id) ? $id['ID'] : -1;
+}
+
+
+function get_genres_of_movie_id(int $id):array
+{
+	$connection = get_db_connection();
+	$genres = mysqli_query($connection,
+		"SELECT NAME FROM genre 
+               WHERE ID IN (SELECT GENRE_ID FROM movie_genre WHERE MOVIE_ID = '{$id}')");
+	$genres = mysqli_fetch_all($genres);
+	return ($genres) ? array_map(fn($genre): string => $genre[0],$genres) : [];
 }
