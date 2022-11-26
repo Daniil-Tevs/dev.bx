316e862eab4644608e4a745e07ca902c9eb67124
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 00:47:06 +0200

    movie_card.php: edit vars to up register

--- views/components/movie_card.php
+++ views/components/movie_card.php
@@ -1,32 +1,31 @@
 <?php
 /**
- * @var int $id
- * @var int $duration
- * @var string $title
- * @var string $release_date
- * @var string $original_title
- * @var string $description
- * @var array $genres
+ * @var int $ID
+ * @var int $DURATION
+ * @var string $TITLE
+ * @var string $RELEASE_DATE
+ * @var string $ORIGINAL_TITLE
+ * @var string $DESCRIPTION
  */
 ?>
 <div class="movie-item">
 	<div class="movie-item-overlay">
-		<a href="<?= "/movie_info.php?id=$id" ?>">Подробнее</a>
+		<a href="<?= "/movie_info.php?id=$ID" ?>">Подробнее</a>
 	</div>
-	<div class="movie-img" style="background-image: url('<?= "/data/images/$id.jpg" ?>')"></div>
+	<div class="movie-img" style="background-image: url('<?= "/data/images/$ID.jpg" ?>')"></div>
 	<div class="movie-info">
-		<h1><?= sprintf('%s(%s)', $title, $release_date) ?></h1>
-		<h2><?= $original_title ?></h2>
+		<h1><?= sprintf('%s(%s)', $TITLE, $RELEASE_DATE) ?></h1>
+		<h2><?= $ORIGINAL_TITLE ?></h2>
 		<div class="movie-description">
-			<?= format_description($description) ?>
+			<?= format_description($DESCRIPTION) ?>
 		</div>
 		<div class="movie-additional-info">
 			<div class="movie-time">
 				<img src='/data/icons/clock.png' alt="Time:">
-				<p><?= sprintf('%s мин. / %s', $duration, format_time($duration)) ?></p>
+				<p><?= sprintf('%s мин. / %s', $DURATION, format_time($DURATION)) ?></p>
 			</div>
 			<div class="movie-genre">
-				<?= implode(', ', array_slice($genres, 0, 3)) ?>
+				<?= implode(', ', array_slice(get_genres_of_movie_id($ID), 0, 3)) ?>
 			</div>
 		</div>
 	</div>
