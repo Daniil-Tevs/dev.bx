826b9ff857d09e01e099de7b6a0e24980282a338
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-26 20:52:43 +0200

    database_service.php: add function get_db_connection

---
+++ services/database_service.php
@@ -0,0 +1,37 @@
+<?php
+
+function get_db_connection()
+{
+	static $connection = null;
+
+	if($connection === null)
+	{
+		$connection = mysqli_init();
+		$host = option('DB_HOST');
+		$username = option('DB_USERNAME');
+		$password = option('DB_PASSWORD');
+		$db_name = option('DB_NAME');
+
+		$connection_result = mysqli_real_connect(
+			$connection,
+			$host,
+			$username,
+			$password,
+			$db_name
+		);
+
+		if (!$connection_result)
+		{
+			$error = mysqli_connect_errno() . ':' . mysqli_connect_error();
+			throw new Exception($error);
+		}
+
+		$encoding_result = mysqli_set_charset($connection, 'utf8');
+		if (!$encoding_result)
+		{
+			throw new Exception(mysqli_error($connection));
+		}
+	}
+
+	return $connection;
+}
