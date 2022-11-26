a4509ec03428d2bf7515993de07d86d383704395
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-26 20:53:00 +0200

    services/index.php: require configuration_service.php and database_service.php

--- services/index.php
+++ services/index.php
@@ -1,6 +1,8 @@
 <?php
 
+require_once ROOT . '/services/configuration_service.php';
 require_once ROOT . '/services/template_service.php';
 require_once ROOT . '/services/movie_service.php';
 require_once ROOT . '/services/genre_service.php';
 require_once ROOT . '/services/http_service.php';
+require_once ROOT . '/services/database_service.php';
