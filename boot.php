188e313e20e6fe2354015665e4a1c28f5f03ee10
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 00:44:05 +0200

    boot.php: edit make menu with mysql

--- boot.php
+++ boot.php
@@ -6,18 +6,17 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/movie/data.php';
 require_once __DIR__ . '/services/index.php';
 
-/**
- * @var array $genres
- */
+
 
 $base_menu = option('BASE_MENU');
 
 $fav_key = array_key_last($base_menu);
 $fav_title = array_pop($base_menu);
 
-foreach ($genres as $url => $genre)
+$genres = get_genres();
+foreach (mysqli_fetch_all($genres) as $genre)
 {
-	$base_menu['?genre=' . $url] = $genre;
+	$base_menu['?genre=' . $genre[1]] = $genre[2];
 }
 
 $base_menu[$fav_key] = $fav_title;
