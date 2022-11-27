a70a2757eebd7a847cc492747411ce1c51697fe6
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 12:11:30 +0200

    genre_service.php: some refactor

--- services/genre_service.php
+++ services/genre_service.php
@@ -4,10 +4,10 @@ function get_genres()
 {
 	$connection = get_db_connection();
 	$genres = mysqli_query($connection, "SELECT * FROM genre");
-	return ($genres) ? $genres : [];
+	return ($genres) ?: [];
 }
 
-function get_genre_id_by_url(string $url):int
+function get_genre_id_by_url(string $url): int
 {
 	$connection = get_db_connection();
 	$id = mysqli_query($connection, "SELECT ID FROM genre WHERE CODE = '$url'");
@@ -15,13 +15,12 @@ function get_genre_id_by_url(string $url):int
 	return ($id) ? $id['ID'] : -1;
 }
 
-
-function get_genres_of_movie_id(int $id):array
+function get_genres_of_movie_id(int $id): array
 {
 	$connection = get_db_connection();
 	$genres = mysqli_query($connection,
 		"SELECT NAME FROM genre 
                WHERE ID IN (SELECT GENRE_ID FROM movie_genre WHERE MOVIE_ID = '{$id}')");
 	$genres = mysqli_fetch_all($genres);
-	return ($genres) ? array_map(fn($genre): string => $genre[0],$genres) : [];
+	return ($genres) ? array_map(fn ($genre): string => $genre[0], $genres) : [];
 }
