ca241aae7a99ba70bc3a3d59c5a1916e7d53444d
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 12:08:03 +0200

    boot.php: remove require movie/data.php and remove create base menu

--- boot.php
+++ boot.php
@@ -3,20 +3,4 @@
 const ROOT = __DIR__;
 
 require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/movie/data.php';
 require_once __DIR__ . '/services/index.php';
-
-
-
-$base_menu = option('BASE_MENU');
-
-$fav_key = array_key_last($base_menu);
-$fav_title = array_pop($base_menu);
-
-$genres = get_genres();
-foreach (mysqli_fetch_all($genres) as $genre)
-{
-	$base_menu['?genre=' . $genre[1]] = $genre[2];
-}
-
-$base_menu[$fav_key] = $fav_title;
