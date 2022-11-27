7dda7f7123aa0fc20c9eb6c714000b082a1e965c
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 12:14:02 +0200

    pages/movie_info.php: refactor for work with database

--- views/pages/movie_info.php
+++ views/pages/movie_info.php
@@ -4,19 +4,22 @@
  */
 ?>
 <div class="content full_details">
-	<?php if ($movies['id'] !== null): ?>
+	<?php
+	if (!empty($movies)): ?>
 		<div class="movie-details">
 			<div class="ru-title">
-				<h1><?= sprintf('%s(%s)', $movies['title'], $movies['release_date']) ?></h1>
+				<h1><?= sprintf('%s(%s)', $movies['TITLE'], $movies['RELEASE_DATE']) ?></h1>
 				<img src="data/icons/heart.png" alt="Heart">
 			</div>
 			<div class="en-title">
-				<h2><?= $movies['original_title'] ?></h2>
-				<div class="min-age"><?= $movies['age_restriction'] ?>&plus;</div>
+				<h2><?= $movies['ORIGINAL_TITLE'] ?></h2>
+				<div class="min-age"><?= $movies['AGE_RESTRICTION'] ?>&plus;</div>
 			</div>
 			<?= view('components/description_part_of_movie', $movies) ?>
 		</div>
-	<?php else: ?>
+	<?php
+	else: ?>
 		<div class="alert alert-find">This movie is don't find</div>
-	<?php endif ?>
+	<?php
+	endif ?>
 </div>
