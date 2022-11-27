f92b170500d22342f5f020b1803ec5c1533f338c
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 12:13:15 +0200

    index.php: add get title from function option, get base_menu from function get_base_menu / add work with data from database

--- public/movie_info.php
+++ public/movie_info.php
@@ -1,28 +1,22 @@
 <?php
 
 require_once __DIR__ . '/../boot.php';
-/**
- * @var string $title
- * @var array $movies
- * @var array $base_menu
- */
+
+$movie = [];
 
 if (isset($_GET['id']))
 {
-	$movie = get_movies_by_id((int)$_GET['id'], $movies);
+	$movie = get_movie_by_id((int)$_GET['id']);
+	$movie = mysqli_fetch_assoc($movie);
 }
 elseif (isset($_GET['genre']))
 {
 	redirect(sprintf('/genre=%s', $_GET['genre']));
 }
-else
-{
-	$movie = [];
-}
 
 echo view('layout', [
-	'title' => $title,
-	'menu' => $base_menu,
+	'title' => option('APP_NAME'),
+	'menu' => get_base_menu(),
 	'content' => view('pages/movie_info', [
 		'movies' => $movie,
 	]),
