71bc14d1d69ec0ade96bfd7991b9614f2ceab833
Daniil-Tevs <daniil_tevs@mail.ru>
2022-11-27 12:12:24 +0200

    services/index.php: require staff_service.php and menu_service.php

--- services/index.php
+++ services/index.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once ROOT . '/services/configuration_service.php';
+require_once ROOT . '/services/database_service.php';
 require_once ROOT . '/services/template_service.php';
 require_once ROOT . '/services/movie_service.php';
 require_once ROOT . '/services/genre_service.php';
+require_once ROOT . '/services/staff_service.php';
+require_once ROOT . '/services/menu_service.php';
 require_once ROOT . '/services/http_service.php';
-require_once ROOT . '/services/database_service.php';
