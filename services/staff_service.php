1adb5b4a0655ef663d70ae9d3dfcb37dc34c03ca
Daniil-Tevs <daniil_tevs@mail.ru>
2022-12-03 23:49:04 +0200

    staff_service.php: remove get_director_name_by_id, refactor

--- services/staff_service.php
+++ services/staff_service.php
@@ -1,18 +1,36 @@
 <?php
 
-function get_director_name_by_id(int $id): string
+function get_actors_by_movie_id(int $id): array
 {
 	$connection = get_db_connection();
-	$director = mysqli_query($connection, "SELECT * FROM director WHERE ID = '$id'");
-	$director = mysqli_fetch_assoc($director);
-	return ($director['NAME']) ?: '';
+	$actors = mysqli_query($connection,
+		"SELECT a.NAME FROM movie mov
+               INNER JOIN actor a ON a.ID in (SELECT ACTOR_ID FROM movie_actor WHERE mov.ID = MOVIE_ID)
+               WHERE mov.ID = $id");
+	if (!$actors)
+	{
+		throw new Exception("Error: getting from db actors by this movie id $id - return false");
+	}
+	$actors = mysqli_fetch_all($actors);
+	return array_map(fn ($actor): string => $actor[0], $actors);
 }
 
-function get_actors_names_by_movie_id(int $movie_id): array
+function get_actors_sorted_by_movies_id(): array
 {
 	$connection = get_db_connection();
-	$inner_query = sprintf("SELECT ACTOR_ID FROM movie_actor WHERE MOVIE_ID = %s", sql_protect_str($movie_id));
-	$actors = mysqli_query($connection, "SELECT NAME FROM actor WHERE ID IN ($inner_query)");
-	$actors = mysqli_fetch_all($actors);
-	return ($actors) ? array_map(fn ($actor): string => $actor[0], $actors) : [];
+	$actors = mysqli_query($connection,
+		"SELECT mov.ID, a.NAME FROM movie mov
+               INNER JOIN actor a ON a.ID in (SELECT ACTOR_ID FROM movie_actor WHERE mov.ID = MOVIE_ID)
+               ORDER BY mov.ID;");
+	if (!$actors)
+	{
+		throw new Exception("Error: getting from db actors sorted by movies id - return false");
+	}
+	$formatted_actors = [];
+	while ($actor = mysqli_fetch_assoc($actors))
+	{
+		$formatted_actors[$actor['ID']][] = $actor['NAME'];
+	}
+	return $formatted_actors;
 }
+
