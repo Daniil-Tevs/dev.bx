2038764393bd5ac1b723155e3bffc651b6fa2750
Daniil-Tevs <daniil_tevs@mail.ru>
2022-12-03 23:44:31 +0200

    public/movie_info.php: refactor

--- public/movie_info.php
+++ public/movie_info.php
@@ -7,17 +7,16 @@ $movie = [];
 if (isset($_GET['id']))
 {
 	$movie = get_movie_by_id((int)$_GET['id']);
-	$movie = mysqli_fetch_assoc($movie);
 }
 elseif (isset($_GET['genre']))
 {
-	redirect(sprintf('/genre=%s', $_GET['genre']));
+	redirect(sprintf('/?genre=%s', $_GET['genre']));
 }
 
 echo view('layout', [
 	'title' => option('APP_NAME'),
 	'menu' => get_base_menu(),
 	'content' => view('pages/movie_info', [
-		'movies' => $movie,
+		'movie' => $movie,
 	]),
 ]);
