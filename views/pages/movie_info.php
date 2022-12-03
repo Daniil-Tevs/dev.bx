30e78d87e2c7f3318607b6b95f2d589d468d0096
Daniil-Tevs <daniil_tevs@mail.ru>
2022-12-03 23:44:55 +0200

    pages/movie_info.php: refactor

--- views/pages/movie_info.php
+++ views/pages/movie_info.php
@@ -1,25 +1,23 @@
 <?php
 /**
- * @var array $movies
+ * @var array $movie
  */
 ?>
 <div class="content full_details">
 	<?php
-	if (!empty($movies)): ?>
+	if (!empty($movie)): ?>
 		<div class="movie-details">
 			<div class="ru-title">
-				<h1><?= sprintf('%s(%s)', $movies['TITLE'], $movies['RELEASE_DATE']) ?></h1>
+				<h1><?= sprintf('%s(%s)', $movie['TITLE'], $movie['RELEASE_DATE']) ?></h1>
 				<img src="data/icons/heart.png" alt="Heart">
 			</div>
 			<div class="en-title">
-				<h2><?= $movies['ORIGINAL_TITLE'] ?></h2>
-				<div class="min-age"><?= $movies['AGE_RESTRICTION'] ?>&plus;</div>
+				<h2><?= $movie['ORIGINAL_TITLE'] ?></h2>
+				<div class="min-age"><?= $movie['AGE_RESTRICTION'] ?>&plus;</div>
 			</div>
-			<?= view('components/description_part_of_movie', $movies) ?>
+			<?= view('components/description_part_of_movie', ['movie' => $movie]) ?>
 		</div>
-	<?php
-	else: ?>
+	<?php else: ?>
 		<div class="alert alert-find">This movie is don't find</div>
-	<?php
-	endif ?>
+	<?php endif ?>
 </div>
