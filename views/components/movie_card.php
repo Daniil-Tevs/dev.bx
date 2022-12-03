0204f0fb12761088acea20950c8e74d793845d65
Daniil-Tevs <daniil_tevs@mail.ru>
2022-12-03 23:44:00 +0200

    movie_card.php: use array $movie instead its inner components

--- views/components/movie_card.php
+++ views/components/movie_card.php
@@ -1,31 +1,26 @@
 <?php
 /**
- * @var int $ID
- * @var int $DURATION
- * @var string $TITLE
- * @var string $RELEASE_DATE
- * @var string $ORIGINAL_TITLE
- * @var string $DESCRIPTION
+ * @var array $movie
  */
 ?>
 <div class="movie-item">
 	<div class="movie-item-overlay">
-		<a href="<?= "/movie_info.php?id=$ID" ?>">Подробнее</a>
+		<a href="<?= "/movie_info.php?id={$movie['ID']}" ?>">Подробнее</a>
 	</div>
-	<div class="movie-img" style="background-image: url('<?= "/data/images/$ID.jpg" ?>')"></div>
+	<div class="movie-img" style="background-image: url('<?= "/data/images/{$movie['ID']}.jpg" ?>')"></div>
 	<div class="movie-info">
-		<h1><?= sprintf('%s(%s)', $TITLE, $RELEASE_DATE) ?></h1>
-		<h2><?= $ORIGINAL_TITLE ?></h2>
+		<h1><?= sprintf('%s(%s)', $movie['TITLE'], $movie['RELEASE_DATE']) ?></h1>
+		<h2><?= $movie['ORIGINAL_TITLE'] ?></h2>
 		<div class="movie-description">
-			<?= format_description($DESCRIPTION) ?>
+			<?= format_description($movie['DESCRIPTION']) ?>
 		</div>
 		<div class="movie-additional-info">
 			<div class="movie-time">
 				<img src='/data/icons/clock.png' alt="Time:">
-				<p><?= sprintf('%s мин. / %s', $DURATION, format_time($DURATION)) ?></p>
+				<p><?= sprintf('%s мин. / %s', $movie['DURATION'], format_time($movie['DURATION'])) ?></p>
 			</div>
 			<div class="movie-genre">
-				<?= implode(', ', array_slice(get_genres_of_movie_id($ID), 0, 3)) ?>
+				<?= implode(', ', array_slice($movie['GENRES'], 0, 3)) ?>
 			</div>
 		</div>
 	</div>
